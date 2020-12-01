607082f3c50eafccb97e650a767356ca6e34f926
Vince Scacchitti <VScacchitti24@gmail.com>
2020-12-01 07:27:32 -0500

    server running and connecting to mongoose.

---
+++ server.js
@@ -0,0 +1,32 @@
+// Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+//logs all requests to the console
+const morgan = require("morgan");
+
+//sets up instance of express
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+// Middleware
+app.use(morgan("dev"));
+app.use(express.static("public")); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//connection to mongoDB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+
+mongoose.connect(MONGODB_URI,{
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
+//Routes
+
+
+//server starts listening
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
